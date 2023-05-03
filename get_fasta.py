636593ae9f2d6112fe5c0332eabd86ddc69bab0e
Alina Skrylnik <97610892+AlyaHarvelle@users.noreply.github.com>
2023-05-03 14:53:20 +0200

    Delete get_fasta.py

--- get_fasta.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-def get_fasta(upac): # a function retrieving a FASTA sequence from ID
-    result = subprocess.run(['ssh', 'echidna', '/software/packages/ncbi-blast/latest/bin/blastdbcmd', '-db', '/db/blastdb/uniprot/uniprot.fasta', '-entry', upac], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-    return result.stdout
